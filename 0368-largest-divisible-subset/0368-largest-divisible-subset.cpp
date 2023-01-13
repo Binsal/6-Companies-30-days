d7e7d6db8175eea09c81837f26a04a8758e3e2fa
Binsal <96328531+Binsal@users.noreply.github.com>
2023-01-13 05:48:26 +0530

    Time: 75 ms (49.38%), Space: 8.8 MB (76.64%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-  
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         vector<int>v;
-        vector<int>hash(nums.size(),0);
-        int last_index=0;
-
+        
         sort(nums.begin(),nums.end());
+        
+        vector<int>hash(nums.size(),0);
+        
         vector<int>dp(nums.size(),1);
+        int last_index=0;
         int maxi=1;
-        for(int i=1;i<nums.size();i++){
+        
+        for(int i=0;i<nums.size();i++){
             hash[i]=i;
+            
             for(int j=0;j<i;j++){
                 if(nums[i]%nums[j]==0 && dp[j]+1>dp[i]){
-                        dp[i]=dp[j]+1;
-                        hash[i]=j;
+                    dp[i]=dp[j]+1;
+                    hash[i]=j;
                 }
-                    if(dp[i]>maxi){
-                        maxi=dp[i];
-                        last_index=i;
-                    }
-         
-          }
-        }
+                
+                if(dp[i]>maxi){
+                    maxi=dp[i];
+                    last_index=i;
+                }
+                
+            }     
+      }
         
         v.push_back(nums[last_index]);
-        while( hash[last_index]!=last_index){
-           last_index=hash[last_index];
+        
+        while(hash[last_index]!=last_index){
+            last_index=hash[last_index];
             v.push_back(nums[last_index]);
-           
         }
         
-        return v;
         
+        return v;
     }
-         
 };
