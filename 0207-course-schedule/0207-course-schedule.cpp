b1525321004d6b4521b3c1fbead319e386e9e8f4
Binsal <96328531+Binsal@users.noreply.github.com>
2023-01-14 13:55:16 +0530

    Time: 22 ms (87.02%), Space: 13.2 MB (90.87%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-   
     bool canFinish(int numCourses, vector<vector<int>>& pre) {
-      
-	   vector<int>adj[numCourses];
-       
-        for(auto it:pre){
-            adj[it[1]].push_back(it[0]);
+        vector<int>adj[numCourses];
+        vector<int>v;
+        
+        for(int i=0;i<pre.size();i++){
+            adj[pre[i][1]].push_back(pre[i][0]);
         }
         
-        vector<int>indegree(numCourses,0);
+        vector<int>indegree(numCourses);
+        queue<int>q;
         
         for(int i=0;i<numCourses;i++){
             for(auto it:adj[i]){
@@ -17,33 +17,28 @@ public:
             }
         }
         
-        queue<int>q;
-        
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.push(i);
             }
         }
-        vector<int>topo;
+        
         
         while(!q.empty()){
             int val=q.front();
-            topo.push_back(val);
+            v.push_back(val);
             q.pop();
             
             for(auto it:adj[val]){
                 indegree[it]--;
+                
                 if(indegree[it]==0){
                     q.push(it);
                 }
             }
         }
         
-        
-        if(topo.size()==numCourses){
-            return true;
-        }
-        
+        if(v.size()==numCourses)return true;
         return false;
     }
 };
