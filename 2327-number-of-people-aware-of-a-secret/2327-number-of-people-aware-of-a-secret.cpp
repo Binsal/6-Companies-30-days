cf33d9671e7f3f8d68caad4907ad5aff2e74dcd8
Binsal <96328531+Binsal@users.noreply.github.com>
2023-01-18 01:24:38 +0530

    Time: 105 ms (12.46%), Space: 7 MB (16.94%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    const int mod=1e9 + 7;
+    int mod=1e9+7;
     
     int peopleAwareOfSecret(int n, int delay, int forget) {
-       vector<int>v(2005,0);
+        vector<int>v(2005,0);
         v[1]=1;
-       
+        
         for(int i=1;i<=n;i++){
             for(int j=i+delay;j<i+forget;j++){
-                v[j]=(v[j]+v[i])%mod;
+                v[j]=(v[i]+v[j])%mod;
             }
         }
         
-        int ans=0;
+        
+        int count=0;
         
         for(int i=max(1,n-forget+1);i<=n;i++){
-            ans=(ans+v[i])%mod;
+            count=(count+v[i])%mod;
         }
         
-        return ans;
+        return count;
     }
-};
+}; 
