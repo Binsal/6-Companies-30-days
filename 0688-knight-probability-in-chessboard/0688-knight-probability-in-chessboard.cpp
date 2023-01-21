50119f814f2da3fffefe98298be4fd737ab3c0ee
Binsal <96328531+Binsal@users.noreply.github.com>
2023-01-21 10:23:19 +0530

    Time: 15 ms (64.75%), Space: 8.7 MB (44.38%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -2,36 +2,28 @@ class Solution {
 public:
    
     double rec(int n,int k,int row,int column,vector<vector<vector<double>>>&dp){
-       
-        if(row<0 || column<0 || row>=n || column>=n){
-            
-            return 0;
-        }
-        
-        
-        if(k==0)return 1;
-        
-         if(dp[row][column][k]!=0) return dp[row][column][k];
-        
          
+         if(row<0 || column<0 || row>=n || column>=n) return 0;
+         if(k==0) return 1;
        
-        double ans=0;
+         if(dp[row][column][k]!=0) return dp[row][column][k];
+               
+         double ans=0;
         
-        ans=rec(n,k-1,row-2,column+1,dp)+
-        rec(n,k-1,row+2,column-1,dp)+//
-        rec(n,k-1,row-2,column-1,dp)+//
-        rec(n,k-1,row+2,column+1,dp)+//
-        rec(n,k-1,row+1,column-2,dp)+//
-        rec(n,k-1,row+1,column+2,dp)+//
-        rec(n,k-1,row-1,column-2,dp)+//
-        rec(n,k-1,row-1,column+2,dp);//
+         ans=rec(n,k-1,row-2,column+1,dp)+
+         rec(n,k-1,row+2,column-1,dp)+//
+         rec(n,k-1,row-2,column-1,dp)+//
+         rec(n,k-1,row+2,column+1,dp)+//
+         rec(n,k-1,row+1,column-2,dp)+//
+         rec(n,k-1,row+1,column+2,dp)+//
+         rec(n,k-1,row-1,column-2,dp)+//
+         rec(n,k-1,row-1,column+2,dp);//
         
+         ans=ans/8.0;
         
-        ans=ans/8.0;
+         dp[row][column][k]=ans;
         
-        dp[row][column][k]=ans;
-        
-        return ans;
+         return ans;
     }
     
     double knightProbability(int n, int k, int row, int column) {
